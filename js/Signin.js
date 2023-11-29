b475b755af35e30a3429438aa3a231bc253bfe8c
[Cody Nguyen] <[codytn@usca.edu]>
2023-11-29 12:25:50 -0500

    Login

--- js/Signin.js
+++ js/Signin.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDZIAVnBvB-GHlaDDO2GbOFjQhVvleb344",
-  authDomain: "database2023test.firebaseapp.com",
-  projectId: "database2023test",
-  storageBucket: "database2023test.appspot.com",
-  messagingSenderId: "352598568614",
-  appId: "1:352598568614:web:b1acd2e65514f8050f8e67",
-  measurementId: "G-6EF9ZJX352"
+  apiKey: "AIzaSyCjeMO612mdgsbSbn_iwmjiUCKj2PB-VuI",
+  authDomain: "database-2023-f2fb8.firebaseapp.com",
+  projectId: "database-2023-f2fb8",
+  storageBucket: "database-2023-f2fb8.appspot.com",
+  messagingSenderId: "550346050899",
+  appId: "1:550346050899:web:dab5b45c1eb706cecaa5a6",
+  measurementId: "G-LW20QZZKXB"
 }; 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -15,8 +15,8 @@ $('#Login').submit(function (e) {
   e.preventDefault();
   // get the user name and password from form
   // You need to change this.
-  var email = 'yilianz4@gmail.com';
-  var password = 'ddsgagafda';
+  var email = "#login";
+  var password = '#pwd';
 
   firebase
     .auth()
@@ -24,7 +24,7 @@ $('#Login').submit(function (e) {
     .then((success) => {
       // Signed in
       // ...
-      console.log('login in');
+      console.log('login in successfully');
       let user = firebase.auth().currentUser;
 
       //user.updateProfile({ displayName: "Not sure" });
@@ -45,3 +45,29 @@ $('#Login').submit(function (e) {
 });
 
 // add  a google login choice here 
+$("#google").click(function(){
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // IdP data available in result.additionalUserInfo.profile.
+      // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  })
+})
